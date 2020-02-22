01e817221d37355492839d15263a4ffdcd6157bd
Asus <Asus@DESKTOP-9UHHHMO>
2020-02-22 20:15:31 +0400

    Добавьте файлы проекта.

---
+++ Player1/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player1
+{
+    class Player
+    {
+        public int HP { get; }
+        public int Gold { get; }
+        public int Damage { get; }
+
+
+        public Player(int hp,int gold,int damage)
+        {
+            HP = hp;
+            Gold = gold;
+            Damage = damage;
+        }
+
+        public void ShowPlayer()
+        {
+            Console.WriteLine("Player has  HP : " + HP);
+            Console.WriteLine("Player has  Gold : " + Gold);
+            Console.WriteLine("Player has  Damage : " + Damage);
+        }
+    }
+}
---
+++ Player1/TestPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player1
+{
+    class TestPlayer
+    {
+        static public void Main(String[] args)
+        {
+
+            Player Vasya = new Player(100, 0, 12);
+            Vasya.ShowPlayer();
+            Console.ReadKey();
+        }
+    }
+}
